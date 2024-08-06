fa9367cc010dff135be01292d7c4673cdadaaa31
fonsski <vladdisslav.vh@gmail.com>
2024-08-06 17:27:46 +0600

    Still working

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,4 +9,7 @@ class ProductController extends Controller
     public function index(){
         return 'This is a product page';
     }
-}
+    public function show(){
+        return 'This is a product page';
+    }
+} 
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 // Main page route
-Route::get('/', function(){
+Route::get('/', function () {
     return view('welcome');
 });
 // Products routes
 Route::get('products', [ProductController::class, 'index'])->name('prodcut.index');
+Route::get('products.show', [ProductController::class, 'show'])->name('product.show');
 // User routes
 Route::get('user', [UserController::class, 'index'])->name('user.index');
+Route::get('user.show', [UserController::class, 'show'])->name('user.show');
 // 
