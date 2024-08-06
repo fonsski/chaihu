f200735a3feb1de1aa4f05b81a34b8449d857d79
fonsski <vladdisslav.vh@gmail.com>
2024-08-06 11:59:19 +0600

    Some work

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
+// Main page route
+Route::get('/', function(){
     return view('welcome');
 });
+// Products routes
+Route::get('products', [ProductController::class, 'index'])->name('prodcut.index');
+// User routes
+Route::get('user', [UserController::class, 'index'])->name('user.index');
+// 
